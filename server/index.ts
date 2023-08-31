59086d4009a3dc9c888512701138b8db5a26a64b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-31 18:35:45 +0200

    Get all todos with drizzle

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,18 @@
 import { publicProcedure, router } from "./trpc";
 
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import Database from "better-sqlite3";
+import { todos } from "./db/schema";
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: "drizzle" });
+
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
+    return await db.select().from(todos).all();
   }),
 });
 
