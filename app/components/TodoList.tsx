5d69061feb1a0634ee9c7645e4931e79a695f99e
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-08-31 18:40:26 +0200

    Add todo

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,12 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { trpc } from "../_trpc/client";
 
 const TodoList = () => {
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({ onSettled: () => getTodos.refetch() });
 
-  return <div>{JSON.stringify(getTodos.data)}</div>;
+  const [content, setContent] = useState("");
+
+  return (
+    <div>
+      {JSON.stringify(getTodos.data)}
+
+      <div className='flex gap-3 items-center'>
+        <label htmlFor='content'>Content</label>
+        <input
+          id='content'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className='flex-grow text-black bg-white rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2'
+        />
+        <button
+          onClick={async () => {
+            if (content.length) {
+              addTodo.mutate(content);
+              setContent("");
+            }
+          }}
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+          Add Todo
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import { todos } from "./db/schema";
+import { z } from "zod";
 
 const sqlite = new Database("sqlite.db");
 const db = drizzle(sqlite);
@@ -13,6 +14,10 @@ migrate(db, { migrationsFolder: "drizzle" });
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
     return await db.select().from(todos).all();
+  }),
+  addTodo: publicProcedure.input(z.string()).mutation(async (opts) => {
+    await db.insert(todos).values({ content: opts.input, done: 0 }).run();
+    return true;
   }),
 });
 
